63b120e6e83fde8404a8280432c62628924bf40e
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-06-01 05:14:23 +0900

    14-feat회원가입-api

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -55,29 +55,4 @@ router.post('/sign-up', async (req, res, next) => {
     }
 });
 
-// access token 미들웨어 추가
-router.get('/', async (req, res, next) => {
-  try {
-    const authenticatedUser = await prisma.user.findUnique({
-      where: { userId: req.userId },
-      select: {
-        userId: true,
-        name: true,
-        email: true,
-        role: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
-
-    if (!authenticatedUser) {
-      return res.status(404).json({ error: 'Not found user' });
-    }
-
-    res.status(200).json(authenticatedUser);
-  } catch (error) {
-    next(error);
-  }
-});
-
 export default router
