1ee4f0636d943b20c54bcca978151daf8629afda
romesway <romesway@gmail.com>
2024-06-01 16:15:34 +0900

    feat/로그인 api refreshToken 추가

--- src/routers/auth.routers.js
+++ src/routers/auth.routers.js
@@ -3,7 +3,6 @@ import bcrypt from 'bcrypt';
 import 'dotenv/config';
 import jwt from 'jsonwebtoken';
 import { prisma } from '../utils/prisma.util.js';
-import { acessTokenValidator } from '../middlewares/require-acess-token.middleware.js';
 
 const router = express.Router();
 
@@ -20,11 +19,31 @@ router.post('/sign-in', async (req, res, next) => {
       return res.status(401).json({ status: 401, message: '인증 정보가 유효하지 않습니다.' });
     }
     // accessToken 생성
-    const data = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET_KEY, {
+    const accessToken = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET_KEY, {
       expiresIn: '12h',
     });
 
-    res.status(200).json({ status: 200, message: 'sign-in successed', data });
+    // refreshToken 생성
+    const refreshToken = jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET_KEY, {
+      expiresIn: '7d',
+    });
+    // refreshToken bcrypt로 해쉬하기
+    // salt round 값 환경변수, 상수로 변경하기
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+    // 서버에 토큰 저장
+    await prisma.refreshToken.upsert({
+      where: { userId: user.id },
+      update: { refreshToken: hashedRefreshToken },
+      create: { userId: user.id, refreshToken: hashedRefreshToken },
+    });
+    res
+      .status(200)
+      .json({
+        status: 200,
+        message: '로그인에 성공하였습니다',
+        data: { accessToken, refreshToken },
+      });
   } catch (err) {
     next(err);
   }
