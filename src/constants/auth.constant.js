7f58e359bb8c9d1f0f97d0e724ce828a812d4daa
jkc-mycode <jkc9709@naver.com>
2024-06-01 16:24:15 +0900

    Feat/sign-up, sign-in joi validation

--- src/constants/auth.constant.js
+++ src/constants/auth.constant.js
@@ -1,4 +1,15 @@
+// 해시에 사용한 salt
 export const HASH_SALT = 10;
 
+// 토큰 유효기간
 export const ACCESS_TOKEN_EXPIRED_IN = '12h';
 export const REFRESH_TOKEN_EXPIRED_IN = '7d';
+
+// 유효성 검사시 비밀번호 최소 길이
+export const PASSWORD_MIN_LENGTH = 6;
+
+// 유효성 검사시 이메일 형식
+export const TLDS = ['com', 'net', 'kr'];
+
+//유효성 검사시 최소 도메인 요소
+export const MIN_DOMAIN_SEGMENTS = 2;
---
+++ src/middlewares/validators/sign-up.validator.middleware.js
@@ -0,0 +1,52 @@
+import Joi from 'joi';
+import { MESSAGES } from '../../constants/message.constant.js';
+import { PASSWORD_MIN_LENGTH, TLDS, MIN_DOMAIN_SEGMENTS } from '../../constants/auth.constant.js';
+import { USER_GENDER } from '../../constants/user-gender.constant.js';
+
+// 회원가입 유효성 검사 Joi 스키마
+export const signUpSchema = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: MIN_DOMAIN_SEGMENTS, tlds: { allow: TLDS } })
+    .required()
+    .messages({
+      'string.base': MESSAGES.USER.COMMON.EMAIL.BASE,
+      'string.empty': MESSAGES.USER.COMMON.EMAIL.REQUIRED,
+      'string.email': MESSAGES.USER.COMMON.EMAIL.EMAIL,
+      'any.required': MESSAGES.USER.COMMON.EMAIL.REQUIRED,
+    }),
+  nickname: Joi.string().required().messages({
+    'string.base': MESSAGES.USER.COMMON.NICKNAME.BASE,
+    'string.empty': MESSAGES.USER.COMMON.NICKNAME.REQUIRED,
+    'any.required': MESSAGES.USER.COMMON.NICKNAME.REQUIRED,
+  }),
+  password: Joi.string().min(PASSWORD_MIN_LENGTH).required().messages({
+    'string.base': MESSAGES.USER.COMMON.PASSWORD.BASE,
+    'string.min': MESSAGES.USER.COMMON.PASSWORD.MIN,
+    'string.empty': MESSAGES.USER.COMMON.PASSWORD.REQUIRED,
+    'any.required': MESSAGES.USER.COMMON.PASSWORD.REQUIRED,
+  }),
+  passwordCheck: Joi.string().min(PASSWORD_MIN_LENGTH).required().messages({
+    'string.base': MESSAGES.USER.COMMON.PASSWORD_CONFIRM.BASE,
+    'string.min': MESSAGES.USER.COMMON.PASSWORD_CONFIRM.MIN,
+    'string.empty': MESSAGES.USER.COMMON.PASSWORD_CONFIRM.REQUIRED,
+    'any.required': MESSAGES.USER.COMMON.PASSWORD_CONFIRM.REQUIRED,
+  }),
+  region: Joi.string().required().messages({
+    'string.base': MESSAGES.USER.COMMON.REGION.BASE,
+    'string.empty': MESSAGES.USER.COMMON.REGION.REQUIRED,
+    'any.required': MESSAGES.USER.COMMON.REGION.REQUIRED,
+  }),
+  age: Joi.number().required().messages({
+    'number.base': MESSAGES.USER.COMMON.AGE.BASE,
+    'number.empty': MESSAGES.USER.COMMON.AGE.REQUIRED,
+    'any.required': MESSAGES.USER.COMMON.AGE.REQUIRED,
+  }),
+  gender: Joi.string()
+    .valid(...Object.values(USER_GENDER))
+    .required()
+    .messages({
+      'string.base': MESSAGES.USER.COMMON.GENDER.BASE,
+      'string.empty': MESSAGES.USER.COMMON.GENDER.REQUIRED,
+      'any.only': MESSAGES.USER.COMMON.GENDER.ONLY,
+    }),
+});
