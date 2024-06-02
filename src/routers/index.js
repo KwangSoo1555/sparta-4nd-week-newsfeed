5c5563ef4e10362089cb86180698cfdc5c65acc4
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-06-03 05:20:27 +0900

    feat/verifiedEmail, add validation verificationCode

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import authRouter from './auth.routers.js';
 import tradeRouter from './trade.router.js';
 import userRouter from './user.router.js';
+import authEmailRouter from './auth-email.router.js';
 
 const router = express.Router();
 
-router.use('/auth', authRouter);
+router.use('/auth', [authRouter, authEmailRouter]);
 router.use('/trade', tradeRouter);
 router.use('/user', userRouter);
 
