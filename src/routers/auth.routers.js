5b08f5edb53280b8e6f98dbf7384f979dfbc524c
Bok Kwang Soo <qhrrhkdtn1555@gmail.com>
2024-06-04 16:04:46 +0900

    feat/complete naver passport

--- src/routers/auth.routers.js
+++ src/routers/auth.routers.js
@@ -9,6 +9,7 @@ import { HTTP_STATUS } from '../constants/http-status.constant.js';
 import { MESSAGES } from '../constants/message.constant.js';
 
 import passport from '../passports/kakao-passport.js';
+import { naverPassport } from '../passports/naver-passport.js';
 
 const router = express.Router();
 
@@ -50,6 +51,42 @@ router.get(
       .json({
         status: 200,
         message: '카카오 로그인에 성공하였습니다.',
+        data: { accessToken, refreshToken },
+      });
+  }
+);
+
+// 네이버 소셜 로그인
+router.get('/naver', naverPassport.authenticate('naver'));
+router.get('/naver/oauth', naverPassport.authenticate('naver', {
+    failureRedirect: '/api/auth/fail',
+  }),
+  async (req, res) => {
+    // accessToken 생성
+    const accessToken = jwt.sign({ id: req.user.id }, process.env.ACCESS_TOKEN_SECRET_KEY, {
+      expiresIn: '12h',
+    });
+
+    // refreshToken 생성
+    const refreshToken = jwt.sign({ id: req.user.id }, process.env.REFRESH_TOKEN_SECRET_KEY, {
+      expiresIn: '7d',
+    });
+    // refreshToken bcrypt로 해쉬하기
+    // salt round 값 환경변수, 상수로 변경하기
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+
+    // 서버에 토큰 저장
+    await prisma.refreshToken.upsert({
+      where: { userId: req.user.id },
+      update: { refreshToken: hashedRefreshToken },
+      create: { userId: req.user.id, refreshToken: hashedRefreshToken },
+    });
+    req.token = { accessToken, refreshToken };
+    res
+      .status(200)
+      .json({
+        status: 200,
+        message: '네이버 로그인에 성공하였습니다.',
         data: { accessToken, refreshToken },
       });
   }
